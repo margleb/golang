1c7d5faa74ea1f4c682192105ee1cb730c88379d
Gleb <margleb.dm@yandex.ru>
2022-05-28 09:45:46 +0300

    Channels

--- main.go
+++ main.go
@@ -5,11 +5,27 @@ import (
 	"log"
 )
 
-func main() {
-	log.Println("Hello")
+var rangeNum int = 10
 
-	// импортируем из пакета helpers
-	var myVar helpers.SomeType
-	myVar.TypeName = "Some Name"
-	log.Println(myVar.TypeName)
+func calculateValue(myChannel chan int) {
+	// получаем любое рандомное число
+	randNum := helpers.RandomNumber(rangeNum)
+	// передаем рандомное число в канал
+	myChannel <- randNum
+}
+
+func main() {
+	// создаем канал
+	intChannel := make(chan int)
+	// закрываем канал после того как функция будет завершена
+	defer close(intChannel)
+
+	// передает канал в функ-цию
+	go calculateValue(intChannel)
+
+	// получаем значение из канала
+	// (слушает канал и кидает в него значение)
+	getNum := <-intChannel
+
+	log.Println("Полученное число из канала:", getNum)
 }
