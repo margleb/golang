1c29227013d0ec22f5ae805eeab25f589392884d
Gleb <margleb.dm@yandex.ru>
2022-05-29 16:15:28 +0300

    Error Checking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -25,11 +26,36 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+// Devide - страница деления
+func Divide(w http.ResponseWriter, r *http.Request) {
+
+	f, err := divideValues(100.0, 0.0)
+
+	if err != nil {
+		fmt.Fprintf(w, "Cannot devide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f devided by %f is %f", 100.00, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+
+	if y <= 0 {
+		err := errors.New("cannot devide by zero")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
 // main is the main application function
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/devide", Divide)
 
 	// пишет в консоль
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
