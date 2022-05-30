645990df88443816b4ae0cc16f7a5fbb4948bb7b
Gleb <margleb.dm@yandex.ru>
2022-05-30 08:25:38 +0300

    Serving HTML Templates

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 // не может быть изменяемым
@@ -11,43 +11,22 @@ const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	renderTemplate(w, "about.page.tmpl")
 }
 
-// addValue adds to integers and return the sum
-// со строчной буквы делает видимость фун-ции только в этом пакете
-func addValues(x, y int) int {
-	return x + y
-}
+func renderTemplate(w http.ResponseWriter, tmpl string) {
 
-// Devide - страница деления
-func Divide(w http.ResponseWriter, r *http.Request) {
-
-	f, err := divideValues(100.0, 0.0)
-
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot devide by 0")
+		fmt.Println("error parsing template:", err)
 		return
 	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f devided by %f is %f", 100.00, 0.0, f))
-}
-
-func divideValues(x, y float32) (float32, error) {
-
-	if y <= 0 {
-		err := errors.New("cannot devide by zero")
-		return 0, err
-	}
-
-	result := x / y
-	return result, nil
 }
 
 // main is the main application function
@@ -55,10 +34,6 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/devide", Divide)
-
-	// пишет в консоль
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
 	_ = http.ListenAndServe(portNumber, nil)
 }
