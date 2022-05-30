27935f7f4d18a2afb8230f8199880aa304ac6ebb
Gleb <margleb.dm@yandex.ru>
2022-05-30 09:45:10 +0300

    Reorganizing our code, and adding some basic styling to pages

--- main.go
+++ main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // не может быть изменяемым
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parseTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
 
 // main is the main application function
 func main() {
