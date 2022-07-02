65d3299b1867b52846049c347672bb61d72df86e
Gleb <margleb.dm@yandex.ru>
2022-07-02 08:17:25 +0300

    Experimenting with sessions

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,6 +29,12 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+
+	// берем ip адрес поль-ля
+	remoteIP := r.RemoteAddr
+	// помещаем его в config c сессиями
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -38,6 +44,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// подготовка данных для передачи в шаблон
 	stringMap := make(map[string]string)
 	stringMap["example"] = "Hello, world"
+
+	// получаем из конфига значение сессии
+	// делаем явное преобразование к строке
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	// добавляем значение сессии в stringMap
+	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
